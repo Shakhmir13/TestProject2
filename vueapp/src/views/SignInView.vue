c92e1eea85cf813e6d3863be3db2e2ad44c17c62
Nurmukhamed Berikbolov <109278086+Nurmukhamed7@users.noreply.github.com>
2023-08-23 18:22:17 +0200

    Created Sign in page without logic

--- vueapp/src/views/SignInView.vue
+++ vueapp/src/views/SignInView.vue
@@ -1,7 +1,31 @@
-<template>
-	<div>Это страница Регистрации</div>
-</template>
+<script setup>
+import Button from 'primevue/button'
+import InputText from 'primevue/inputtext'
+</script>
 
-<script setup></script>
+<template>
+	<h2>Sign up</h2>
+	<form class="flex flex-column gap-3">
+		<div class="p-inputgroup flex-1">
+			<span class="p-inputgroup-addon">
+				<i class="pi pi-user"></i>
+			</span>
+			<InputText type="email" placeholder="Your email" />
+		</div>
+		<div class="p-inputgroup flex-1">
+			<span class="p-inputgroup-addon">
+				<i class="pi pi-at"></i>
+			</span>
+			<InputText type="password" placeholder="Password" />
+		</div>
+		<div class="flex flex-column gap-3">
+			<Button label="Submit" />
+			<span
+				>Bro, are you already registered?
+				<router-link to="/signin">Sign in</router-link>
+			</span>
+		</div>
+	</form>
+</template>
 
 <style lang="scss" scoped></style>
