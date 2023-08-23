a188dcfb9d0c0205f96e60540ca3cc5c00356fa5
Nurmukhamed Berikbolov <109278086+Nurmukhamed7@users.noreply.github.com>
2023-08-23 17:01:33 +0200

    Added PrimeVue with CSS

--- vueapp/src/main.js
+++ vueapp/src/main.js
@@ -1,5 +1,13 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import 'primeflex/primeflex.css'
+import 'primeicons/primeicons.css'
+import 'primevue/resources/primevue.min.css'
+import 'primevue/resources/themes/lara-light-indigo/theme.css'
 
-createApp(App).mount('#app')
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import './style.css'
+
+import PrimeVue from 'primevue/config'
+
+createApp(App).use(router).use(PrimeVue).mount('#app')
---
+++ vueapp/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Button from 'primevue/button'
+</script>
+
+<template>
+	<div>Добро пожаловать Нурик</div>
+	<Button label="Submit" />
+</template>
+
+<style scoped></style>
