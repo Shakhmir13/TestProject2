bd6270565217581c3526537a453d66cdb3410d7b
Nurmukhamed Berikbolov <109278086+Nurmukhamed7@users.noreply.github.com>
2023-08-31 23:12:48 +0200

    Нет получения от сервера accounts/refresh-token
    newTokens data: undefined

--- vueapp/src/api.js
+++ vueapp/src/api.js
@@ -10,8 +10,35 @@ const HTTP = axios.create({
 HTTP.interceptors.request.use(config => {
 	const authStore = useAuthStore()
 	const token = authStore.userInfo.token
-	config.headers.Authorization = `Bearer ${token}`
+	if (token) {
+		// TODO: убрать 'v' не забыть
+		config.headers.Authorization = `Bearer ${token + 'v'}`
+	}
 	return config
 })
+
+HTTP.interceptors.response.use(
+	response => {
+		return response
+	},
+	async function (error) {
+		const authStore = useAuthStore()
+		const originalRequest = error.config
+		if (error.response.status === 401 && !originalRequest._retry) {
+			originalRequest._retry = true
+			try {
+				const newTokens = await HTTP.post('accounts/refresh-token', {
+					refreshToken: JSON.parse(localStorage.getItem('userTokens'))
+						.refreshToken,
+					token: JSON.parse(localStorage.getItem('userTokens')).token,
+				})
+
+				console.log('newTokens data:', newTokens)
+			} catch (err) {
+				console.log('ЭТО ошибка в newTokens', err)
+			}
+		}
+	}
+)
 
 export default HTTP
