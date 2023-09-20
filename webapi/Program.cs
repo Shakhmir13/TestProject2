17c097e4cecca72afcd3502d5f5c5fbe74437f73
Shakhmir <paltushev97@mail.ru>
2023-09-20 09:40:52 +0600

    \

--- webapi/Program.cs
+++ webapi/Program.cs
@@ -93,7 +93,7 @@ builder.Services.AddScoped<ITokenService, TokenService>();
 builder.Services.AddScoped<IEmailSender, EmailSender>();
 
 var app = builder.Build();
-app.UseCors(builder => builder.WithOrigins("http://161.97.110.154:3000/", "http://localhost:5175/").AllowCredentials().AllowAnyHeader().AllowAnyMethod());
+app.UseCors(builder => builder.WithOrigins("http://161.97.110.154:3000", "http://localhost:5175").AllowCredentials().AllowAnyHeader().AllowAnyMethod());
 app.UseCookiePolicy();
 app.UseSession();
 app.UseSwagger();
