87d2f7fe6a1caeebdb401d99dc3c803ad5a41d9e
Shakhmir <paltushev97@mail.ru>
2023-09-22 17:17:17 +0600

    добавил модели Кроссовок, Категорий, Производителей

---
+++ TestProject2.DbContext/Repository/ManufacturerRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestProject2.Models;
+
+namespace TestProject2.DbContext.Repository
+{
+    public class ManufacturerRepository : Repository<Manufacturer>
+    {
+        private ApplicationDbContext _context;
+        public ManufacturerRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
