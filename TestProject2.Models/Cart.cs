25e1057b0bf76b27e5e34674da523ddcb62c4648
Shakhmir13 <134289235+Shakhmir13@users.noreply.github.com>
2023-11-30 17:56:59 +0600

    Update Cart.cs

--- TestProject2.Models/Cart.cs
+++ TestProject2.Models/Cart.cs
@@ -9,8 +9,8 @@ namespace TestProject2.Models
     public class Cart
     {
         public int Id { get; set; }
-        public string UserId { get; set; }
-        public ApplicationUser User { get; set; }
+        public string ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
         public int ProductId { get; set; }
         public Shoes Shoes { get; set; }
         public int count { get; set; }
